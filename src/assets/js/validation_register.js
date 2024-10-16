9008cea55f09828307f18d01025547ae89530e20
toan0212 <toando0212@gmail.com>
2024-10-16 22:49:38 +0700

    comit

--- src/assets/js/validation_register.js
+++ src/assets/js/validation_register.js
@@ -7,32 +7,12 @@ function validatePassword(password) {
     return password.length >= 8
 }
 
-document.querySelector('.login-form')?.addEventListener('submit', function(event) {
-    event.preventDefault()
-    const email = document.getElementById('username').value
-    const password = document.getElementById('password').value
-
-    if (!validateEmail(email)) {
-        alert('Please enter a valid email address.')
-        return
-    }
-
-    if (!validatePassword(password)) {
-        alert('Wrong email or password');
-        return;
-    }
-
-    alert('Login successful!');
-    console.log('Redirecting to main.html');
-    window.location.href = "main.html"
-})
-
-document.querySelector('.register-form')?.addEventListener('submit', function(event) {
+document.querySelector('.register-form').addEventListener('submit', function(event) {
     event.preventDefault(); 
-
-    const email = document.getElementById('username').value;
+    const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     const confirmPassword = document.getElementById('confirm-password').value;
+
 
     if (!validateEmail(email)) {
         alert('Please enter a valid email address.');
@@ -48,6 +28,7 @@ document.querySelector('.register-form')?.addEventListener('submit', function(ev
         alert('Passwords do not match.');
         return;
     }
+    
 
     alert('Register successful!');
     window.location.href = "main.html"
